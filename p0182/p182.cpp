ccb22b44a63921621759d8baf575bab03c6d4ada
Guillaume Lempereur <guillaume.lempereur@hotmail.com>
2025-12-01 18:51:39 +0100

    182 cleaned

--- p0182/p182.cpp
+++ p0182/p182.cpp
@@ -1,5 +1,7 @@
-//P182
-//
+/*
+ * p182
+ * quite Brute force : try every e expecting those 5 m are removing the non optimal e
+*/
 #include <iostream>
 #include <vector>
 #include <iomanip>
@@ -7,11 +9,6 @@
 #include <cstdlib>
 #include <ctime>
 
-// not the answer but quite close
-// check middle
-/*
- * quite Brute force : try every e expecting those 5 m are removing the non optimal e
-*/
 
 long long expMod(unsigned long long base, unsigned long long exp, unsigned long long mod) {
 	unsigned long long res = 1;
@@ -25,8 +22,6 @@ long long expMod(unsigned long long base, unsigned long long exp, unsigned long 
 }
 
 int main(int args, char **argv){
-	//std::cout << std::fixed << std::setprecision(6);
-
 	unsigned long long p = 1009, q = 3643;
 	int BL_NB = 5;
 	unsigned long long blackList[5] = {3642, 33210, 73282, 116575, 1225936};
@@ -49,7 +44,6 @@ int main(int args, char **argv){
 	else
 		lim = phi;
 	int ccc = 0;
-	std::cout << "Start " << std::endl;
 	for(e=3;e<lim;e+=2){
 		if(std::gcd(e,phi)!=1)
 			continue;
@@ -65,28 +59,12 @@ int main(int args, char **argv){
 			continue;
 
 		int cc = 0;
-		//es[es_length++] = e;
-		//e_cc[e] = 0;
 		
-		/*
-		//for(m=0;m<n/2;m++){
-		for(m=n/3;m<n/2;m++){
-			c = expMod(m, e, n);
-			if(c==m){
-				std::cout << m << " ";
-				//e_unc[i+1].push_back(m);
-				cc++;
-			}
-		}
-
-		std::cout << std::endl;
-		std::cout << e << " " << cc << std::endl;
-		*/
 		e_tt += e;
 		ccc++;
 	}
-	std::cout << e_tt << std::endl;
 	std::cout << ccc << std::endl;
+	std::cout << "Ans: " << e_tt << std::endl;
 
-  return 0;
+	return 0;
 }
